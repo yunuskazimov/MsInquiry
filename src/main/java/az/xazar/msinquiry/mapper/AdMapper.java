8e21dd5dec08f983a6b41b5c1f1de55961be248a
yunuskazimov <yunus.kazimov@gmail.com>
2021-12-21 00:23:06 +0400

    add mapstruct Mapper for Ad

--- src/main/java/az/xazar/msinquiry/mapper/AdMapper.java
+++ src/main/java/az/xazar/msinquiry/mapper/AdMapper.java
@@ -1,2 +1,20 @@
-package az.xazar.msinquiry.mapper;public class AdMapper {
+package az.xazar.msinquiry.mapper;
+
+import az.xazar.msinquiry.entity.AdEntity;
+import az.xazar.msinquiry.model.Ad.AdDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+public interface AdMapper {
+
+    AdMapper INSTANCE = Mappers.getMapper(AdMapper.class);
+
+    AdDto entityToDto(AdEntity entity);
+
+//    @Mapping(target = "createdAt", ignore = true)
+//    @Mapping(target = "updatedAt", ignore = true)
+//    AdEntity dtoToEntity(AdDto dto);
 }
--- src/main/java/az/xazar/msinquiry/model/Ad/AdDto.java
+++ src/main/java/az/xazar/msinquiry/model/Ad/AdDto.java
@@ -1,5 +1,6 @@
 package az.xazar.msinquiry.model.Ad;
 
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
